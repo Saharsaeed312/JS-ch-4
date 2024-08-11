d81033dffab03e0e900acbb734bd68ade9bd1c98
Responsive-web <saharsaeed8090@gmail.com>
2024-08-11 04:33:50 -0700

    JS-ch-2

---
+++ script.js
@@ -0,0 +1,46 @@
+// chapter:no:4
+// question:no:1
+var age;
+var year;
+var date;
+
+// question:no:2
+// 5 Legal Variable name
+var firstName;
+var last_name;
+var Full$Name;
+var _birthDate;
+var $person_name;
+// 5 Illegal Variable name
+// var 1firstUser;  // number can't be written in the first of variable name.
+// var &modelName; // symbols can't be used in teh starting of variable name.
+// var user id;  //space is not allowed between varibale name.
+// var class;  //Variable name cant be keyword or reserved words.
+// var car*name;  //you can't use any special symbol b/w the variable.Variable name must be started with alphabatic chracter and underscore
+
+// question:no:3
+var rules_for_JS = ("Rules for naming JS variables" + "<br/>" + "<br/>");
+document.write(rules_for_JS)
+// b:
+var variable_rules =
+  "Variable names can only contain  _ , $ and number For example: $my_1st variable " +
+  "<br/>" +
+  "<br/>";
+document.write(variable_rules);
+
+// c:
+var vari_start_with =
+  "Variables must begin with a letter, $ or _ . For example $name, _name or name" +
+  "<br/>" +
+  "<br/>";
+document.write(vari_start_with);
+
+// d:
+var variable_cases = "Variable names are case sensitive " + "<br/>" + "<br/>";
+document.write(variable_cases);
+
+// e:
+var variable_name =
+  "Variable names should not be JS keywords." + "<br/>" + "<br/>";
+document.write(variable_name);
+
